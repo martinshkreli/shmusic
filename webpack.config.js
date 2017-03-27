71c606ecfdef63264b6b731cc4a904153d806fda
Ricky Muldrew <rickymuldrew@gmail.com>
2017-03-26 22:24:09 -0400

    config webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,56 @@
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var OpenBrowserPlugin = require('open-browser-webpack-plugin');
+var WebpackNotifierPlugin = require('webpack-notifier');
+var port = process.env.PORT || 3000;
+var pathFunc = function (str) {
+  return path.resolve(__dirname, str);
+};
+
 module.exports = {
+  devServer: {
+    historyApiFallback: true,
+    hot: true,
+    inline: true,
+    progress: true,
+    contentBase: './src',
+    port: port,
+  },
   entry: [
-    './source/App.js'
+    'webpack/hot/dev-server',
+    'webpack-dev-server/client?http://localhost:' + port,
+    pathFunc('src/app.jsx'),
   ],
   output: {
-    path: __dirname,
-    filename: "bundle.js"
+    path: pathFunc('build'),
+    publicPath: '/',
+    filename: './bundle.js'
   },
   module: {
     loaders: [{
-      test: /\.jsx?$/,
-      loader: 'babel-loader',
-      query: {
-        presets: ['es2015', 'react'],
-      }
-    }]
-  }
+      test: /\.scss$/,
+      include: pathFunc('src'),
+      loaders: ['style', 'css', 'sass'],
+    }, {
+      test: /\.js[x]?$/,
+      include: pathFunc('src'),
+      exclude: /node_modules/,
+      loaders: ['babel'],
+    }, ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'ShMusic',
+      template: 'src/index.html',
+    }),
+    new WebpackNotifierPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new OpenBrowserPlugin({
+      url: 'http://localhost:' + port,
+    }),
+  ]
 };
