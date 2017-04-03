e0b4ece9cee98da53bb144540eb567018228913e
Ricky Muldrew <rickymuldrew@gmail.com>
2017-04-02 23:12:22 -0400

    (basic) render of keys

--- src/containers/Keyboard/Keyboard.jsx
+++ src/containers/Keyboard/Keyboard.jsx
@@ -1,31 +1,43 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import autobind from 'autobind-decorator'
-// import {} from './config';
+import generate from './config';
 import { playNote, stopNote } from '../../modules';
 import { Key } from '../../components';
 
+
+@connect(state => ({ 
+  activeNote: state.instrument.activeNote
+}), { playNote, stopNote })
 @autobind
-@connect(state => ({ activeNote: state.instrument.activeNote }), { playNote, stopNote })
 class Keyboard extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      keys: generate.keys,
+    };
   }
   handleKeys(key, i) {
     const { activeNote, playNote, stopNote } = this.props;
+    const { note, number } = key;
+
     return (
-      {/*<key
-        accidental={}
-        active={activeNote === note}
-        note={}
+      <Key
+        key={i}
+        accidental={~note.indexOf('#')}
+        active={number === activeNote}
+        note={number}
         onKeyDown={playNote}
         onKeyUp={stopNote}
-      >*/}
+      />
     );
   }
   render() {
-    console.log(this.props);
-    return (<div />);
+    return (
+      <ul className="keyboard">
+        {this.state.keys.map(this.handleKeys)}
+      </ul>
+    );
   }
 }
 
